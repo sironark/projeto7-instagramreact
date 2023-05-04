9045ab86977ca9f04bc659ef917e9bfc2fe053a2
Gabriel Oliveira <gabriel_lopeslomeu@hotmail.com>
2023-05-04 19:51:01 -0300

    versão com map no stories

--- src/Stories.js
+++ src/Stories.js
@@ -1,11 +1,80 @@
 
+const arrayStories = [
+  {
+    src:"assets/img/9gag.svg",
+    alt: '9gag',
+    usuario:'9gag'
+  },
+  {
+    src:'assets/img/meowed.svg',
+    alt: 'meowed',
+    usuario:'meowed'
+  },
+  {
+    src:'assets/img/barked.svg',
+    alt: 'barked',
+    usuario:'barked'
+  },
+  {
+    src:'assets/img/nathanwpylestrangeplanet.svg',
+    alt: 'nathanwpylestrangeplanet',
+    usuario:'nathanwpylestrangeplanet'
+  },
+  {
+    src:'assets/img/wawawicomics.svg',
+    alt: 'wawawicomics',
+    usuario:'wawawicomics'
+  },
+  {
+    src:'assets/img/respondeai.svg',
+    alt: 'respondeai',
+    usuario:'respondeai'
+  },
+  {
+    src:'assets/img/filomoderna.svg',
+    alt: 'filomoderna',
+    usuario:'filomoderna'
+  },
+  {
+    src:'assets/img/memeriagourmet.svg',
+    alt: 'memeriagourmet',
+    usuario:'memeriagourmet'
+  }
+
+];
+
+
+
 
 export function Stories(){
     return(
         <div>
             <div class="stories">
            
-            <div class="story">
+            {arrayStories.map(story =>
+              <div class="story">
+              <div class="imagem">
+                <img src={story.src} alt={story.alt}/>
+              </div>
+              <div class="usuario">
+                {story.usuario} 
+              </div>
+            </div>
+              )}
+  
+            <div class="setinha">
+              <ion-icon name="chevron-forward-circle"></ion-icon>
+            </div>
+          </div>
+
+
+        </div>
+    );
+}
+
+
+
+/*  <div class="story">
               <div class="imagem">
                 <img src="assets/img/9gag.svg" alt="9gag"/>
               </div>
@@ -61,22 +130,12 @@ export function Stories(){
         <div class="usuario">
           filomoderna
         </div>
-      </div>
-      <div class="story">
-        <div class="imagem">
-          <img src="assets/img/memeriagourmet.svg" alt="memeriagourmet"/>
-        </div>
-        <div class="usuario">
-          memeriagourmet
-        </div>
-      </div>    
-  
-            <div class="setinha">
-              <ion-icon name="chevron-forward-circle"></ion-icon>
             </div>
-          </div>
-
-
-        </div>
-    );
-}
+            <div class="story">
+              <div class="imagem">
+                <img src="assets/img/memeriagourmet.svg" alt="memeriagourmet"/>
+              </div>
+              <div class="usuario">
+                memeriagourmet
+              </div>
+            </div>   */
